b8aa398af5438d31a6643ba6cdab95e8ae10cd81
U-Yk45 <kyurafam@gmail.com>
2025-06-07 20:45:05 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_007</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       $product = array('name' => 'onion', 'price' => 200, 'weight' => 160);
+
+       print_r($product); 
+
+       ?>
+   </p>
+</body>
+
+</html>
