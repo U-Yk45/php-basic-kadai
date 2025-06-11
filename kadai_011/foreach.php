0f01fc34c3b021c4aba39d551b637b35bb3dcf4a
U-Yk45 <kyurafam@gmail.com>
2025-06-11 21:34:54 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,21 @@
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>kadai_007</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $vegetable = array('名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道');
+
+    foreach ($vegetable as $key => $value) {
+      echo $key . "：" . $value . "<br>";
+    }
+   ?>
+
+  </p>
+</body>
+
+</html>
